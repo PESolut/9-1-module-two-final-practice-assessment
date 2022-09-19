163bc21184615fa41574636c0e8ada7153b3be2b
PESolut <jahaadpetty@pursuit.org>
2022-09-19 09:35:51 -0400

    sections 1,2,3,4 created and appeneded, sections 1,2 partially populated with no functionality

--- main.js
+++ main.js
@@ -6,6 +6,16 @@ const body = document.querySelector('body')
 const header = document.createElement('header')
 const headerImg = document.createElement('img')
 const headerH1 = document.createElement('h1')
+const main = document.createElement('main')
+const mainSec1 = document.createElement('section')
+const mainSec2 = document.createElement('section')
+const mainSec3 = document.createElement('section')
+const mainSec4 = document.createElement('section')
+const mainSec1H2 = document.createElement('h2')
+const mainSec1Select = document.createElement('select')
+const mainSec1Hr = document.createElement('hr')
+const mainSec2H4 = document.createElement('h4')
+const mainSec2Form = document.createElement('form')
 
 // classes
 headerImg.classList.add('header')
@@ -13,16 +23,31 @@ headerImg.classList.add('image')
 headerH1.classList.add('header')
 headerH1.classList.add('h1')
 header.classList.add('center')
-
+mainSec1Hr.classList.add('center')
+mainSec2H4.classList.add('itallic')
 
 // appends
 body.append(header)
 header.append(headerImg)
 header.append(headerH1)
+body.append(main)
+main.append(mainSec1)
+mainSec1.append(mainSec1H2)
+mainSec1.append(mainSec1Select)
+mainSec1.append(mainSec1Hr)
+main.append(mainSec2)
+mainSec2.append(mainSec2H4)
+mainSec2.append(mainSec2Form)
+main.append(mainSec3)
+main.append(mainSec4)
+
 
 // attributes 
 headerImg.src = 'assets/emojilaugh-emoji.gif'
 headerH1.textContent = 'Random People App'
+mainSec1H2.textContent =  'Select Person'
+mainSec2H4.textContent = 'Add a Shoutout'
+
 
 fetch(`${baseURL}${peopleURL}`)
     .then((res) => res.json())

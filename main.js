d8960b051db0183cdd7c16fe1d040797607930a4
PESolut <jahaadpetty@pursuit.org>
2022-09-18 20:00:03 -0400

    header created, text content added and styled

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const baseURL = 'https://ghibliapi.herokuapp.com'
 const peopleURL = '/people'
 
-// selectors
+// selectors | elements
 const body = document.querySelector('body')
+const header = document.createElement('header')
+const headerImg = document.createElement('img')
+const headerH1 = document.createElement('h1')
+
+// classes
+headerImg.classList.add('header')
+headerImg.classList.add('image')
+headerH1.classList.add('header')
+headerH1.classList.add('h1')
+
+// appends
+body.append(header)
+header.append(headerImg)
+header.append(headerH1)
+
+// attributes 
+headerImg.src = 'assets/emojilaugh-emoji.gif'
+headerH1.textContent = 'Random People App'
 
 fetch(`${baseURL}${peopleURL}`)
     .then((res) => res.json())
